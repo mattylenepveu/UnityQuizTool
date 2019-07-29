3254719fc2423acfc283f66f5eaca784a518a4e0
Matthew Le Nepveu <matthew@lenepveu.com>
2019-07-29 15:32:57 +1000

    I setup the canvas and UI today, and coded some basic logic which is yet to be applied to the quiz yet.

---
+++ QuizTool/Assets/Scripts/Question.cs
@@ -0,0 +1,6 @@
+﻿[System.Serializable]
+public class Question
+{
+    public string m_strQuestion;
+    public bool m_bIsTrue;
+}
---
+++ QuizTool/Assets/Scripts/QuizManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class QuizManager : MonoBehaviour
+{
+    public Question[] m_questions;
+    private static List<Question> m_unanswered;
+
+    private Question m_current;
+
+    private void Start()
+    {
+        if (m_unanswered == null || m_unanswered.Count == 0)
+        {
+            m_unanswered = m_questions.ToList<Question>();
+        }
+
+        GetRandomQuestion();
+        Debug.Log(m_current.m_strQuestion + " is " + m_current.m_bIsTrue);
+    }
+
+    private void GetRandomQuestion()
+    {
+        int nRandomQuestionIndex = Random.Range(0, m_unanswered.Count);
+        m_current = m_unanswered[nRandomQuestionIndex];
+
+        m_unanswered.RemoveAt(nRandomQuestionIndex);
+    }
+}
