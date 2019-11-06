73baf1fea591d5a61ad9434e3cc093b6b1654360
mattylenepveu <matthew@lenepveu.com>
2019-11-06 17:45:43 +1100

    Started implementing the feature that allows users to load questions in to Unity using a text document, although a lot of work needs to be done by dissecting the questions in the text document into a C# questions list.

---
+++ QuizTool/Assets/Scripts/LoadQuestions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LoadQuestions : MonoBehaviour
+{
+    public string m_strData = "Questions";
+    private string m_strContents;
+    private TextAsset m_txtAsset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_txtAsset = (TextAsset)Resources.Load(m_strData);
+        m_strContents = m_txtAsset.text;
+    }
+
+    // Update is called once per frame
+    void OnGUI()
+    {
+        GUILayout.Label(m_strContents);
+    }
+
+    public void ReadTextFile()
+    {
+        // Split questions into a list 
+
+        // Initiate for loop that iterates through for the amount of questions
+
+        // Make sure the text file has questions in it
+
+        // Add questions from text file to questions array one by one
+    }
+}
